a3d499a26dca3ebfd487eac64f296c9e418bde17
Andrew Dotterer <adotterer@gmail.com>
2020-10-08 11:22:42 -0700

    add red/black

--- connect-four.js
+++ connect-four.js
@@ -1,26 +1,55 @@
-import { Game } from './game.js';
+import { Game } from "./game.js";
 
 let game = undefined;
+let clickTarget = document.getElementById("click-targets");
 
-window.addEventListener('DOMContentLoaded', () => {
-    let player1 = document.getElementById('player-1-name');
-    let player2 = document.getElementById('player-2-name');
-    let newGameButton = document.getElementById('new-game');
+function updateUI() {
+  let boardHolder = document.getElementById("board-holder");
+  if (game === undefined) {
+    boardHolder.classList.add("is-invisible");
+  } else {
+    boardHolder.classList.remove("is-invisible");
+    document.getElementById("game-name").innerHTML = game.getName();
+  }
+  let currentPlayer = game.currentPlayer;
+  if (currentPlayer === 1) {
+    clickTarget.classList.remove("black");
+    clickTarget.classList.add("red");
+  } else {
+    clickTarget.classList.remove("red");
+    clickTarget.classList.add("black");
+  }
+}
 
-    player1
-        .addEventListener('keyup', (event) => {
-            enableNewGameButton();
-        })
-    player2
-        .addEventListener('keyup', (event) => {
-            enableNewGameButton();
-        })
+window.addEventListener("DOMContentLoaded", () => {
+  let player1 = document.getElementById("player-1-name");
+  let player2 = document.getElementById("player-2-name");
+  let newGameButton = document.getElementById("new-game");
 
-    function enableNewGameButton() {
-        let player1content = player1.value;
-        let player2content = player2.value;
-        newGameButton.disabled = (player1content.length === 0 ||
-                                  player2content.length === 0)
-    };
+  player1.addEventListener("keyup", (event) => {
+    enableNewGameButton();
+  });
+  player2.addEventListener("keyup", (event) => {
+    enableNewGameButton();
+  });
 
-})
+  function enableNewGameButton() {
+    let player1content = player1.value;
+    let player2content = player2.value;
+    newGameButton.disabled =
+      player1content.length === 0 || player2content.length === 0;
+  }
+
+  newGameButton.addEventListener("click", (event) => {
+    game = new Game(player1.value, player2.value);
+    player1.value = "";
+    player2.value = "";
+    enableNewGameButton();
+
+    updateUI();
+  });
+  clickTarget.addEventListener("click", (event) => {
+    game.playInColumn();
+    updateUI();
+  });
+});
--- game.js
+++ game.js
@@ -1,12 +1,19 @@
 export class Game {
-    constructor(player1name, player2name) {
-        this.player1name = player1name;
-        this.player2name = player2name;
+  constructor(player1name, player2name) {
+    this.player1name = player1name;
+    this.player2name = player2name;
+    this.currentPlayer = 1;
+  }
+
+  getName() {
+    return `${this.player1name} vs ${this.player2name}`;
+  }
+
+  playInColumn() {
+    if (this.currentPlayer === 1) {
+      this.currentPlayer = 2;
+    } else {
+      this.currentPlayer = 1;
     }
-
-    getName () {
-        return `${this.player1name} vs ${this.player2name}`
-    }
-
-
+  }
 }
